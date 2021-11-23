7378cb53a2590bd9d84f754abe4d604467f3000c
nabeelnoor <nabeelnoor914@gmail.com>
2021-11-23 21:52:36 +0500

    working version

--- src/App.js
+++ src/App.js
@@ -1,26 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
-import Web3 from 'web3'
+import React, { Component } from "react";
+import Web3 from "web3";
+import { SIMP_STORAGE_ABI, SIMP_STORAGE_ADDRESS } from './config'
+class App extends Component {
+  constructor() {
+    super()
+    this.state = { account: '', simpcontract: '', value: '' }
+  }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  componentDidMount() {
+    this.loadBlockChainData()
+  }
+
+  async loadBlockChainData() {
+    const web3 = new Web3(Web3.givenProvider || "http://localhost:7545")
+    const accounts = await web3.eth.getAccounts()
+    this.setState({ account: accounts[0] })
+    const simpstorage = new web3.eth.Contract(SIMP_STORAGE_ABI, SIMP_STORAGE_ADDRESS);
+    console.log(simpstorage)
+    this.setState({ simpcontract: simpstorage })
+    const value = await simpstorage.methods.get().call()
+    this.setState({ value: value })
+  }
+
+  render() {
+    // return (
+    //   // <h1>{this.state.account}</h1>
+
+    //   <h1>Current Value of Stored Data:{this.state.value}</h1>
+    // );
+
+    return (
+      < div>
+        <h1>Your account: {this.state.account}</h1>
+        <h1>The value of X: {this.state.value}</h1>
+        < form onSubmit={(event) => {
+          event.preventDefault()
+          const valueField = new FormData(event.target).get("valueField");
+          this.state.simpcontract.methods.set(valueField).send({
+            from:
+              this.state.account
+          })
+          // this.loadBlockchainData()
+        }} >
+          <input id="field" name="valueField" type="text" required />
+          <br/>
+          < input type="submit" hidden="" />
+        </ form>
+      </ div>
+    );
+  }
+};
 
 export default App;
---
+++ src/config.js
@@ -0,0 +1,29 @@
+export const SIMP_STORAGE_ADDRESS = '0x7113cDcB15f226E02e72e233dBF7bC2F7059f01c'
+export const SIMP_STORAGE_ABI = [
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "x",
+          "type": "uint256"
+        }
+      ],
+      "name": "set",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "get",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ]
