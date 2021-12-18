682178e60941127b54d4875b02be990077e19889
Nabeel Noor <nabeelnoor914@gmail.com>
2021-12-19 04:28:21 +0500

    account is created with password,call and send methods,

--- src/App.js
+++ src/App.js
@@ -23,23 +23,78 @@ class App extends Component {
     this.setState({ value: value })
   }
 
-  
+
   render() {
-    const getTotalAccount=async ()=>{
+    const getTotalAccount = async () => {
       const web3 = new Web3("http://localhost:7545")
       const accounts = await web3.eth.getAccounts()
-      console.log("Length:",accounts.length)
+      console.log("Length:", accounts.length)
       console.log(accounts)
     }
-    const createAccount=()=>{
+    const createAccount = () => {
       //this will create account from scratch
-      
-      // let accounts = new Accounts('http://localhost:7545');
-      // var Accounts = require('web3-eth-accounts');
       const web3 = new Web3("http://localhost:7545")
-      let temp=web3.eth.accounts.create();
-      console.log(temp)
+      //1.method 1 will create account but not store to ganache
+      // let temp = web3.eth.accounts.create();
+      // console.log(temp)
 
+      //2.method 2 store to ganace
+      let finalResult = web3.eth.personal.newAccount("password")
+        .then((result) => {
+          console.log(result)
+        })
+      console.log(finalResult)
+
+    }
+
+    const doTransaction = () => {
+      const web3 = new Web3("http://localhost:7545")
+      //this will sign transaction and display result not updated to blockChain
+      let tempo = web3.eth.accounts.signTransaction({
+        to: '0x15895791421fe43Cb601e7bb50498e9bFACC20f1',
+        value: '1000000000',
+        gas: 2000000
+      }, '0xbcffa1518a9408727849802aef7a6bf53d5e7f99c05625773c47e41bb811c928').then(console.log)
+      // console.log(tempo)
+    }
+
+    const callContract = async () => {
+      const web3 = new Web3("http://localhost:7545")
+      //perfectly working with the blockChain to call contract
+      let Contract = require('web3-eth-contract');
+      Contract.setProvider("http://localhost:7545");
+      let contract = new Contract(SIMP_STORAGE_ABI, SIMP_STORAGE_ADDRESS);  //get the instance of contract
+      //call to not payable function
+      contract.methods.get()
+        .call({ from: '0x172DDf7F7f8e1fFf5f04ba1b8Cd8Ed5242D43e73' },
+          function (error, result) {
+            console.log(result)
+          });
+
+
+      // call to payable function
+      //you have to unlock account
+      web3.eth.personal.unlockAccount('0x172DDf7F7f8e1fFf5f04ba1b8Cd8Ed5242D43e73', 'password', 50000).then(
+        () => {
+          contract.methods.set(150)
+            .send({ from: '0x172DDf7F7f8e1fFf5f04ba1b8Cd8Ed5242D43e73' })
+            .on('receipt', function (receipt) {
+              console.log(receipt)
+              web3.eth.personal.lockAccount('0x172DDf7F7f8e1fFf5f04ba1b8Cd8Ed5242D43e73') //now again lock the account
+            });
+        }
+      )
+      // console.log(tempo)
+
+      // await web3.eth.personal.lockAccount('0x172DDf7F7f8e1fFf5f04ba1b8Cd8Ed5242D43e73')
+
+
+    }
+
+    const deployContract = () => {
+      // var Contract = require('web3-eth-contract');
+      // Contract.setProvider("http://localhost:7545");
+      // var contract = new Contract(jsonInterface, address);
 
     }
     // return (
@@ -62,12 +117,14 @@ class App extends Component {
           // this.loadBlockchainData()
         }} >
           <input id="field" name="valueField" type="text" required />
-          <br/>
+          <br />
           < input type="submit" hidden="" />
         </ form>
 
         <button onClick={createAccount}>create Account</button>
         <button onClick={getTotalAccount}>getTotalAccount</button>
+        <button onClick={doTransaction}>doTransaction</button>
+        <button onClick={callContract}>callContract</button>
       </ div>
     );
   }
