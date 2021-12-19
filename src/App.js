5401a628b537e9a72570a11d1f0f0c9967ff89b7
Nabeel Noor <nabeelnoor914@gmail.com>
2021-12-20 04:04:10 +0500

    removing deployed fuction

--- src/App.js
+++ src/App.js
@@ -13,19 +13,19 @@ class App extends Component {
   }
 
   async loadBlockChainData() {
-    const web3 = new Web3("http://localhost:7545")
-    const accounts = await web3.eth.getAccounts()
-    this.setState({ account: accounts[0] })
-    const simpstorage = new web3.eth.Contract(SIMP_STORAGE_ABI, SIMP_STORAGE_ADDRESS);
-    console.log(simpstorage)
-    this.setState({ simpcontract: simpstorage })
-    const value = await simpstorage.methods.get().call()
-    this.setState({ value: value })
+    // const web3 = new Web3("http://localhost:7545")
+    // const accounts = await web3.eth.getAccounts()
+    // this.setState({ account: accounts[0] })
+    // const simpstorage = new web3.eth.Contract(SIMP_STORAGE_ABI, SIMP_STORAGE_ADDRESS);
+    // console.log(simpstorage)
+    // this.setState({ simpcontract: simpstorage })
+    // const value = await simpstorage.methods.get().call()
+    // this.setState({ value: value })
   }
 
 
   render() {
-    
+
     const getTotalAccount = async () => {
       const web3 = new Web3("http://localhost:7545")
       const accounts = await web3.eth.getAccounts()
@@ -53,7 +53,7 @@ class App extends Component {
       const web3 = new Web3("http://localhost:7545")
       //this will sign transaction and display result not updated to blockChain
       let tempo = web3.eth.accounts.signTransaction({
-        to: '0x15895791421fe43Cb601e7bb50498e9bFACC20f1',
+        to: '0x64647B792c7B31904E7a12c3857A54893a8c9ADb',
         value: '1000000000',
         gas: 2000000
       }, '0xbcffa1518a9408727849802aef7a6bf53d5e7f99c05625773c47e41bb811c928').then(console.log)
@@ -67,22 +67,23 @@ class App extends Component {
       Contract.setProvider("http://localhost:7545");
       let contract = new Contract(SIMP_STORAGE_ABI, SIMP_STORAGE_ADDRESS);  //get the instance of contract
       //call to not payable function
-      contract.methods.get()
-        .call({ from: '0x172DDf7F7f8e1fFf5f04ba1b8Cd8Ed5242D43e73' },
+      contract.methods.getTrafficRules()
+        .call({ from: '0x64647B792c7B31904E7a12c3857A54893a8c9ADb' },
           function (error, result) {
             console.log(result)
           });
 
 
-      // call to payable function
+      // call to payable function and sending 2 wei
       //you have to unlock account
-      web3.eth.personal.unlockAccount('0x172DDf7F7f8e1fFf5f04ba1b8Cd8Ed5242D43e73', 'password', 50000).then(
+      web3.eth.personal.unlockAccount('0x64647B792c7B31904E7a12c3857A54893a8c9ADb', 'password', 50000).then(
         () => {
-          contract.methods.set(150)
-            .send({ from: '0x172DDf7F7f8e1fFf5f04ba1b8Cd8Ed5242D43e73' })
+          contract.methods.test()
+            .send({ from: '0x64647B792c7B31904E7a12c3857A54893a8c9ADb', value: web3.utils.toWei('2', 'ether') })
             .on('receipt', function (receipt) {
               console.log(receipt)
-              web3.eth.personal.lockAccount('0x172DDf7F7f8e1fFf5f04ba1b8Cd8Ed5242D43e73') //now again lock the account
+              web3.eth.personal.lockAccount('0x64647B792c7B31904E7a12c3857A54893a8c9ADb') //now again lock the account
+
             });
         }
       )
@@ -93,23 +94,14 @@ class App extends Component {
 
     }
 
-    const deployContract = () => {
+    const getAccountBalance = async () => {
       const web3 = new Web3("http://localhost:7545")
-      //perfectly working with the blockChain to call contract
-      let Contract = require('web3-eth-contract');
-      Contract.setProvider("http://localhost:7545");
-      let contract = new Contract(SIMP_STORAGE_ABI, SIMP_STORAGE_ADDRESS);  //get the instance of contract
-      console.log(contract.options.jsonInterface)
-      // let Contract = require('web3-eth-contract');
-      // Contract.setProvider("http://localhost:7545");
-
-      
-
+      web3.eth.getBalance('0x64647B792c7B31904E7a12c3857A54893a8c9ADb').then(value => console.log(value.toString()))
     }
-    
+
     return (
       < div>
-        <h1>Your account: {this.state.account}</h1>
+        {/* <h1>Your account: {this.state.account}</h1>
         <h1>The value of X: {this.state.value}</h1>
         < form onSubmit={(event) => {
           event.preventDefault()
@@ -123,13 +115,13 @@ class App extends Component {
           <input id="field" name="valueField" type="text" required />
           <br />
           < input type="submit" hidden="" />
-        </ form>
+        </ form> */}
 
         <button onClick={createAccount}>create Account</button>
         <button onClick={getTotalAccount}>getTotalAccount</button>
         <button onClick={doTransaction}>doTransaction</button>
         <button onClick={callContract}>callContract</button>
-        <button onClick={deployContract}>deployContract</button>
+        <button onClick={getAccountBalance}>getBalance</button>
       </ div>
     );
   }
--- src/config.js
+++ src/config.js
@@ -1,29 +1,554 @@
-export const SIMP_STORAGE_ADDRESS = '0x7113cDcB15f226E02e72e233dBF7bC2F7059f01c'
+export const SIMP_STORAGE_ADDRESS = ''
 export const SIMP_STORAGE_ABI = [
-    {
-      "inputs": [
-        {
-          "internalType": "uint256",
-          "name": "x",
-          "type": "uint256"
-        }
-      ],
-      "name": "set",
-      "outputs": [],
-      "stateMutability": "nonpayable",
-      "type": "function"
-    },
-    {
-      "inputs": [],
-      "name": "get",
-      "outputs": [
-        {
-          "internalType": "uint256",
-          "name": "",
-          "type": "uint256"
-        }
-      ],
-      "stateMutability": "view",
-      "type": "function"
-    }
-  ]
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "name": "ActiveChallan",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "LicenseID",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "CitizenName",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "CitizenCNIC",
+        "type": "string"
+      },
+      {
+        "internalType": "enum ChallanSystem.VehicleType",
+        "name": "CarType",
+        "type": "uint8"
+      },
+      {
+        "internalType": "string",
+        "name": "CarNumberPlate",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "timeStamp",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "Fine",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bool",
+        "name": "status",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "ChallanHistory",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "LicenseID",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "CitizenName",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "CitizenCNIC",
+        "type": "string"
+      },
+      {
+        "internalType": "enum ChallanSystem.VehicleType",
+        "name": "CarType",
+        "type": "uint8"
+      },
+      {
+        "internalType": "string",
+        "name": "CarNumberPlate",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "timeStamp",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "Fine",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bool",
+        "name": "status",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "Owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "RuleList",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "Description",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "ACategoryVehicle_Fine",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "BCategoryVehicle_Fine",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "CCategoryVehicle_Fine",
+        "type": "uint256"
+      },
+      {
+        "internalType": "bool",
+        "name": "status",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "TP",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "testStatus",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "testValue",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "_TP",
+        "type": "address"
+      }
+    ],
+    "name": "addTrafficPolice",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_description",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_Afine",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_Bfine",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_Cfine",
+        "type": "uint256"
+      }
+    ],
+    "name": "addTrafficRule",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_ruleID",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_Afine",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_Bfine",
+        "type": "uint256"
+      },
+      {
+        "internalType": "uint256",
+        "name": "_Cfine",
+        "type": "uint256"
+      }
+    ],
+    "name": "updatePriceForRule",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_ruleID",
+        "type": "uint256"
+      }
+    ],
+    "name": "deactivateRule",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_ruleID",
+        "type": "uint256"
+      }
+    ],
+    "name": "activateRule",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_LicenseID",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_Name",
+        "type": "string"
+      },
+      {
+        "internalType": "string",
+        "name": "_CNIC",
+        "type": "string"
+      },
+      {
+        "internalType": "enum ChallanSystem.VehicleType",
+        "name": "_CarType",
+        "type": "uint8"
+      },
+      {
+        "internalType": "string",
+        "name": "_carPlate",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256[]",
+        "name": "_voilatedRules",
+        "type": "uint256[]"
+      },
+      {
+        "internalType": "string",
+        "name": "_timeStamp",
+        "type": "string"
+      }
+    ],
+    "name": "addChallan",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_LicenseID",
+        "type": "string"
+      }
+    ],
+    "name": "getCurrentChallan",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "string",
+            "name": "LicenseID",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "CitizenName",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "CitizenCNIC",
+            "type": "string"
+          },
+          {
+            "internalType": "enum ChallanSystem.VehicleType",
+            "name": "CarType",
+            "type": "uint8"
+          },
+          {
+            "internalType": "string",
+            "name": "CarNumberPlate",
+            "type": "string"
+          },
+          {
+            "internalType": "uint256[]",
+            "name": "VoilationCode",
+            "type": "uint256[]"
+          },
+          {
+            "internalType": "string",
+            "name": "timeStamp",
+            "type": "string"
+          },
+          {
+            "internalType": "uint256",
+            "name": "Fine",
+            "type": "uint256"
+          },
+          {
+            "internalType": "bool",
+            "name": "status",
+            "type": "bool"
+          }
+        ],
+        "internalType": "struct ChallanSystem.Challan",
+        "name": "",
+        "type": "tuple"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_LicenseID",
+        "type": "string"
+      }
+    ],
+    "name": "getAllChallan",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "string",
+            "name": "LicenseID",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "CitizenName",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "CitizenCNIC",
+            "type": "string"
+          },
+          {
+            "internalType": "enum ChallanSystem.VehicleType",
+            "name": "CarType",
+            "type": "uint8"
+          },
+          {
+            "internalType": "string",
+            "name": "CarNumberPlate",
+            "type": "string"
+          },
+          {
+            "internalType": "uint256[]",
+            "name": "VoilationCode",
+            "type": "uint256[]"
+          },
+          {
+            "internalType": "string",
+            "name": "timeStamp",
+            "type": "string"
+          },
+          {
+            "internalType": "uint256",
+            "name": "Fine",
+            "type": "uint256"
+          },
+          {
+            "internalType": "bool",
+            "name": "status",
+            "type": "bool"
+          }
+        ],
+        "internalType": "struct ChallanSystem.Challan[]",
+        "name": "",
+        "type": "tuple[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getTrafficRules",
+    "outputs": [
+      {
+        "components": [
+          {
+            "internalType": "string",
+            "name": "Description",
+            "type": "string"
+          },
+          {
+            "internalType": "uint256",
+            "name": "ACategoryVehicle_Fine",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "BCategoryVehicle_Fine",
+            "type": "uint256"
+          },
+          {
+            "internalType": "uint256",
+            "name": "CCategoryVehicle_Fine",
+            "type": "uint256"
+          },
+          {
+            "internalType": "bool",
+            "name": "status",
+            "type": "bool"
+          }
+        ],
+        "internalType": "struct ChallanSystem.VoilationRule[]",
+        "name": "",
+        "type": "tuple[]"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "test",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "payable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_LicenseID",
+        "type": "string"
+      }
+    ],
+    "name": "payChallan",
+    "outputs": [],
+    "stateMutability": "payable",
+    "type": "function"
+  }
+]
