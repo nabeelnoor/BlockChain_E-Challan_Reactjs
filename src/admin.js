cc39e382b0ddf2e6b168305568b679f0645e2a87
Nabeel Noor <nabeelnoor914@gmail.com>
2021-12-20 17:59:54 +0500

    adding navbar

--- src/admin.js
+++ src/admin.js
@@ -8,6 +8,7 @@ import Button from '@mui/material/Button';
 import { useState } from 'react';
 import { Link } from "react-router-dom";
 import CustomPopup from "./CustomPopup"
+import Sidenav from './sidenav';
 // import { TextField } from '@mui/material';
 
 function Admin() {
@@ -21,6 +22,7 @@ function Admin() {
 
   return (
     <div>
+      <Sidenav name0="Admin Panel" name1="Add Traffic Rule" name2="Show Traffic Rule"  name3="Add Traffic Officer" name4="Show Traffic Officer" link0="admin" link1="addTR" link2="showTR" link3="addTP" link4="showTP"/>
       {/* {"Admin"}
       <br></br>
       { localStorage.getItem('id')}
@@ -32,12 +34,12 @@ function Admin() {
         duration={8} transition={2} />
 
       <div>
-        <h1 style={{ color: 'white', paddingLeft: '50px', paddingTop: '60px' }}>Challan Payment</h1>
-        <p style={{ color: 'whitesmoke', paddingLeft: '50px', marginBottom: '100px', fontSize: 25 }}>
+        <h1 style={{ color: 'white', paddingLeft: '550px', paddingTop: '60px' }}>Challan Payment</h1>
+        <p style={{ color: 'whitesmoke', paddingLeft: '350px', marginBottom: '100px', fontSize: 25 }}>
           An End-End Encrypted Fee Challan Payment Application</p>
-        <Button variant="contained" style={{ marginLeft: '20px' }} onClick={(e) => {setVisibility(!visibility); setDecider(2)}}>Admin Panel</Button>
+        {/* <Button variant="contained" style={{ marginLeft: '20px' }} onClick={(e) => {setVisibility(!visibility); setDecider(2)}}>Admin Panel</Button>
         <Button variant="contained" style={{ marginLeft: '50px' }} onClick={(e) => {setVisibility(!visibility); setDecider(1)}}>Traffic Officer Panel</Button>
-        <Button variant="contained" style={{ marginLeft: '70px' }} onClick={(e) => {setVisibility(!visibility); setDecider(0)}}>Citizen Panel</Button>
+        <Button variant="contained" style={{ marginLeft: '70px' }} onClick={(e) => {setVisibility(!visibility); setDecider(0)}}>Citizen Panel</Button> */}
        
       
       
