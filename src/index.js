81d8684a50b29aae0e37f78d8f75d75eb1d10043
Nabeel Noor <nabeelnoor914@gmail.com>
2021-12-20 22:17:25 +0500

    router for citizens

--- src/index.js
+++ src/index.js
@@ -14,6 +14,9 @@ import GetTR from './GetTR';
 import Police from './Police';
 import PShowTR from './PShowTR';
 import AddChallan from './AddChallan';
+import CShowTR from './CShowTR';
+import CShowChallanHistory from './CShowChallanHistory';
+import CPayChallan from './CPayChallan';
 
 
 
@@ -36,10 +39,14 @@ class App extends React.Component {
         <Route path="admin/addTP" caseSensitive={false} element={<AddTP />} />
         <Route path="admin/addTR" caseSensitive={false} element={<AddTR />} />
         <Route path="/admin/showTR" caseSensitive={false} element={<GetTR />} />
-        <Route path="citizen" caseSensitive={false} element={<Citizen />} />
         <Route path="police" caseSensitive={false} element={<Police />} />
         <Route path="police/showTR" caseSensitive={false} element={<PShowTR />} />
         <Route path="police/addChallan" caseSensitive={false} element={<AddChallan />} /> 
+        <Route path="citizen" caseSensitive={false} element={<Citizen />} />
+        <Route path="citizen/CShowTR" caseSensitive={false} element={<CShowTR />} />
+
+        <Route path="citizen/CChallanHistory" caseSensitive={false} element={<CShowChallanHistory />} />
+        <Route path="citizen/CPayChallan" caseSensitive={false} element={<CPayChallan />} />
         
         
       </Routes>
